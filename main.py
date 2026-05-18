c88bac3dc6af4131d5b8a0b51f517545fea0794f
GitHub User <user@github.com>
2026-05-18 18:26:53 +0530

    added status_code to get_headers

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def create_book(book_data:BookCreateModel):
         "author":book_data.author
     }
 
-@app.get('/get_headers')
+@app.get('/get_headers',status_code=200)
 async def get_headers(
     accept:str=Header(None),
     content_type:str=Header(None),
