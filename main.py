c778bd898fc77fc1214889fa50a09a00ad783699
GitHub User <user@github.com>
2026-05-18 18:44:53 +0530

    books as database and created one get request which gets all books in books dict

--- main.py
+++ main.py
@@ -1,42 +1,82 @@
-from fastapi import FastAPI,Header
-from typing import Optional
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 app=FastAPI()
 
 
-@app.get('/')
-async def read_root():
-    return {"message":"Hello world"}
+books = [
+    {
+        "id": 1,
+        "title": "Think Python",
+        "author": "Allen B. Downey",
+        "publisher": "O'Reilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 2,
+        "title": "Django By Example",
+        "author": "Antonio Mele",
+        "publisher": "Packt Publishing Ltd",
+        "published_date": "2022-01-19",
+        "page_count": 1023,
+        "language": "English",
+    },
+    {
+        "id": 3,
+        "title": "The web socket handbook",
+        "author": "Alex Diaconu",
+        "publisher": "Xinyu Wang",
+        "published_date": "2021-01-01",
+        "page_count": 3677,
+        "language": "English",
+    },
+    {
+        "id": 4,
+        "title": "Head first Javascript",
+        "author": "Hellen Smith",
+        "publisher": "Oreilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 540,
+        "language": "English",
+    },
+    {
+        "id": 5,
+        "title": "Algorithms and Data Structures In Python",
+        "author": "Kent Lee",
+        "publisher": "Springer, Inc",
+        "published_date": "2021-01-01",
+        "page_count": 9282,
+        "language": "English",
+    },
+    {
+        "id": 6,
+        "title": "Head First HTML5 Programming",
+        "author": "Eric T Freeman",
+        "publisher": "O'Reilly Media",
+        "published_date": "2011-21-01",
+        "page_count": 3006,
+        "language": "English",
+    },
+]
 
-@app.get('/greet')
-async def greet_name(name:Optional[str]="User",
-                     age:int=0) -> dict:
-    return {"message":f"hello {name}","age":age}
 
+@app.get('/books')
+async def get_all_books():
+    return books
 
-class BookCreateModel(BaseModel):
-    title:str
-    author:str
+@app.post('/books')
+async def create_a_book() -> dict:
+    pass
 
+@app.get('book/{book_id}')
+async def get_book(book_id:int) -> dict:
+    pass
 
-@app.post('/create_book')
-async def create_book(book_data:BookCreateModel):
-    return {
-        "title":book_data.title,
-        "author":book_data.author
-    }
+@app.get('book/{book_id}')
+async def update_book(book_id:int) -> dict:
+    pass
 
-@app.get('/get_headers',status_code=200)
-async def get_headers(
-    accept:str=Header(None),
-    content_type:str=Header(None),
-    user_agent:str=Header(None),
-    Host:str=Header(None)
-):
-    request_headers={}
-    request_headers['Accept']=accept
-    request_headers['Content-Type']=content_type
-    request_headers['User-Agent']=user_agent
-    request_headers['Host']=Host
-    return request_headers
+@app.get('book/{book_id}')
+async def delete_book(book_id:int) -> dict:
+    pass
