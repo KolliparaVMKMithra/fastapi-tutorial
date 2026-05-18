6c1729d8c1745b66180241f6b536b656c3e9a225
GitHub User <user@github.com>
2026-05-18 19:33:10 +0530

    added delete request method

--- main.py
+++ main.py
@@ -110,6 +110,11 @@ async def update_book(book_id:int,book_update_data:BookUpdateModel) -> dict:
               return book
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Book with id {book_id} not found')
 
-@app.delete('/book/{book_id}')
-async def delete_book(book_id:int) -> dict:
-    pass
+@app.delete('/book/{book_id}',status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id:int):
+    for book in books:
+        if book['id']==book_id:
+            books.remove(book)
+            return {}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Book with id {book_id} not found')
+         
